95f8916b26136b62dd305bed8c5b5df82b89c489
thilinavp <thilinavp@gmail.com>
2023-06-07 02:11:03 +0530

    google auth sign in and database created

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import Link from "next/link"
 import Image from "next/image"
@@ -7,16 +7,16 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 import Provider from "./Provider";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
   const [providers, setProviders ] = useState(null);
   const [toggleDropdown, setToggleDropdown ] = useState(false);
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
-    setProviders();
+    setUpProviders();
   }, [])
   
 
@@ -33,10 +33,11 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+      
       {/* desktop navigation */}
 
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt"
             className="black_btn">
@@ -49,7 +50,7 @@ const Nav = () => {
 
             <Link href="/">
               <Image 
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -77,10 +78,10 @@ const Nav = () => {
 
       {/* mobile navigation  */}
           <div className="sm:hidden flex relative">
-          {isUserLoggedIn ? (
+          {session?.user ? (
             <div className="flex">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
